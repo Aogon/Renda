dee8ae76e65007a5ecde17b89719bf7e128be442
Aogon <aoi_sakai@icloud.com>
2020-05-12 18:53:48 +0900

    add new file

---
+++ app/src/main/java/app/sakai/tororoimo/renda/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.sakai.tororoimo.renda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
